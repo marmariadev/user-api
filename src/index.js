a95095b13a8df080f6c31732212baed9b9871272
marmariadev <marmariadev@gmail.com>
2024-03-08 18:41:21 -0300

    Add dotenv

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const app = require('./app');
+const { PORT } = require('./config/environment');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
 });
